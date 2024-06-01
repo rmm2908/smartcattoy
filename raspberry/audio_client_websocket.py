620474fa99ce855daae2056a7221fbe0f54363b2
Kilian Stöckler <35853679+ProxyBlack61@users.noreply.github.com>
2024-06-01 22:52:59 +0200

    Feature/audio (#15)
    * Implemented audioclient
    - Can play static audio files
    - Can also receive audio files and play them
    
    * Implemented Ionic app funtion
    - Added new buttons and file upload in the app
    - Made the audioClient safer for wrong filenames
    - Added sound queue, lets sound finish before it plays the next

---
+++ raspberry/audio_client_websocket.py
@@ -0,0 +1,99 @@
+import websockets
+import pygame
+import asyncio
+import json
+import time
+import os
+import tempfile
+import base64
+
+
+pygame.mixer.init()
+
+def play_audio(audio_file_path):
+    pygame.mixer.music.load(audio_file_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        time.sleep(1/1000)
+
+    pygame.mixer.music.unload()
+    print(f"Audio file unloaded: {audio_file_path}")
+
+async def receive_file(fileName, fileData):
+    if os.path.exists(fileName):
+        os.remove(fileName)
+
+    with open(fileName, "wb") as file:
+        file.write(base64.b64decode(fileData))
+        print(f"File received: {fileName}")
+
+def handle_audio_command(content_obj):
+    if "play_audio" in content_obj:
+        try:
+            audio_FileName = sanitize_filename(content_obj["play_audio"])
+            print(f"Received audio file: {audio_FileName}")
+            audio_path = os.path.join(os.curdir, audio_FileName)
+            if os.path.exists(audio_path):
+                play_audio(audio_path)
+                print(f"Playing audio: {audio_path}")
+        except ValueError as e:
+            print(f"Error: {e}")
+
+def get_rotating_filename(base_path, base_name, extension, limit=10):
+    index_file_path = os.path.join(base_path, f"{base_name}_index.txt")
+    
+    if os.path.exists(index_file_path):
+        with open(index_file_path, 'r') as file:
+            last_index = int(file.read().strip())
+    else:
+        last_index = 0
+
+    next_index = (last_index % limit) + 1
+
+    with open(index_file_path, 'w') as file:
+        file.write(str(next_index))
+
+    filename = f"{base_name}_{next_index}.{extension}"
+    return os.path.join(base_path, filename)
+
+
+def sanitize_filename(filename):
+    allowed_extensions = {'.mp3', '.wav'}
+    
+    filename = filename.strip()
+    filename = filename.replace('/', '').replace('\\', '').replace('\0', '')
+    filename = os.path.basename(filename)
+    
+    root, ext = os.path.splitext(filename)
+    if ext.lower() not in allowed_extensions:
+        raise ValueError(f"Unauthorized file extension: {ext}")
+    
+    max_length = 50
+    if len(filename) > max_length:
+        raise ValueError(f"Filename too long. Max length is {max_length} characters.")
+    
+    return filename
+
+async def handler():
+    uri = "ws://localhost:3000"
+    async with websockets.connect(uri, max_size=20000000) as websocket:
+        await websocket.send("{\"request\": \"message from raspberry audio client\"}")
+        done = False
+        while not done:
+            time.sleep(10/1000)
+            message = await websocket.recv()
+            print(message)
+            if isinstance(message, str):
+                try:  
+                    parsed_message = json.loads(message)
+                    if "play_audio" in parsed_message:
+                        handle_audio_command(parsed_message)
+                    elif "operation" in parsed_message and parsed_message["operation"] == "file_transfer":
+                        file_name = get_rotating_filename(os.curdir, "audioFile", "mp3")
+                        await receive_file(file_name, parsed_message["file"])
+                        play_audio(file_name)
+                except ValueError:
+                    print("Could not parse message")
+                    continue
+
+asyncio.run(handler())
